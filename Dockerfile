6ad95fda68e0da75bb1f31c9a6af5b11bf139b64
kaoutarM-18 <fouadikaoutar18@gmail.com>
2026-05-04 23:03:55 +0100

    dokerFileAdd

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM php:8.2-cli
+
+RUN apt-get update && apt-get install -y \
+    unzip \
+    curl \
+    git \
+    libzip-dev \
+    zip
+
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+WORKDIR /app
+
+COPY . .
+
+RUN composer install --no-dev --optimize-autoloader
+
+RUN chmod -R 777 storage bootstrap/cache
+
+EXPOSE 10000
+
+CMD php -S 0.0.0.0:10000 -t public
--- routes/web.php
+++ routes/web.php
@@ -46,29 +46,6 @@ Route::get('/api/modules/{id_filiere}', function ($id_filiere) {
     return \App\Models\Module::where('id_filiere', $id_filiere)->get();
 });
 
-// Routes Enseignant
-// Route::prefix('enseignant')->name('enseignant.')->group(function () {
-//     Route::get('/seances/{seance}/presences',[App\Http\Controllers\Enseignant\PresenceController::class, 'show'])->name('presences.show');
-//     Route::get('/seances/{seance}/export-pdf',[App\Http\Controllers\Enseignant\PresenceController::class, 'exportPDF'])->name('presences.export.pdf');
-//     Route::get('/cours/{cours}/rapport',[App\Http\Controllers\Enseignant\PresenceController::class, 'rapportCours'])->name('presences.rapport');
-//     Route::get('/cours/{cours}/rapport/export-pdf',  [App\Http\Controllers\Enseignant\PresenceController::class, 'exportRapportPDF'])->name('presences.rapport.pdf');
-    
-    
-//     // Route::get('/dashboard', [CoursController::class, 'dashboard'])->name('dashboard');
-//     Route::middleware(['auth', 'nocache'])->group(function () {
-//     Route::get('/prof/dashboard', [CoursController::class, 'dashboard']);
-//     });
-    
-//     Route::resource('cours', CoursController::class);
-//     Route::get('/cours/{coursId}/seance/create', [CoursController::class, 'createSeance'])->name('seances.create');
-//     Route::post('/seances', [CoursController::class, 'storeSeance'])->name('seances.store');
-//     Route::get('/generer-qr/{seance}', [CoursController::class, 'showSeanceQR'])->name('seances.qr');
-//     Route::get('/download-qr/{seance}', [CoursController::class, 'downloadQR'])->name('seances.download-qr');
-//     Route::get('/modules/{id}/details', [CoursController::class, 'moduleDetails'])
-//     ->name('modules.details');
-    
-//     });
-
 Route::prefix('enseignant')->name('enseignant.')->group(function () {
 
     Route::middleware(['auth', 'nocache'])->group(function () {
@@ -119,25 +96,11 @@ Route::prefix('enseignant')->name('enseignant.')->group(function () {
 });
 
 
-
-// Routes Étudiant
-// Route::prefix('etudiant')->name('etudiant.')->group(function () {
-
-//    Route::middleware(['auth', 'nocache'])->group(function () {
-//     Route::get('/dashboard', [PresenceController::class, 'dashboard']);
-//     });
-
-//     //Route::get('/dashboard', [PresenceController::class, 'dashboard'])->name('dashboard');
-    
-//     //Route::get('/scanner/{seance}', [PresenceController::class, 'scan'])->name('presences.scan');
-//     Route::get('/valider-presence', [PresenceController::class, 'store'])->name('presences.store');
-// });
-
 Route::prefix('etudiant')->name('etudiant.')->group(function () {
 
     Route::middleware(['auth', 'nocache'])->group(function () {
 
-        Route::get('/dashboard', [PresenceController::class, 'dashboard']);
+        Route::get('/dashboard', [PresenceController::class, 'dashboard'])->name('dashboard');
 
         Route::get('/valider-presence', [PresenceController::class, 'store'])->name('presences.store');
         // Route::get('/scanner/{seance}', [PresenceController::class, 'scan'])->name('presences.scan');
