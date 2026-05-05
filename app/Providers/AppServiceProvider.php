2276e55d9cbadea3169c9c5f27d7f51bda306b71
Fatimazahrasaad <fatosao78@gmail.com>
2026-05-05 12:38:29 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,8 +21,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
-    {
-        //
+   public function boot()
+{
+    if (env('APP_ENV') === 'production') {
+        \URL::forceScheme('https');
     }
+}
 }
